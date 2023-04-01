2b4cd346b5509f1a8ccc7ff293929c0bf81349bd
robertkottelin <robert.kottelin@gmail.com>
2023-04-01 16:48:03 +0200

    Update queue.rs

--- src/data_structures/queue.rs
+++ src/data_structures/queue.rs
@@ -6,3 +6,49 @@ pub fn queue() -> VecDeque<i32> {
     queue.push_back(2);
     queue
 }
+
+
+pub struct Queue<T> {
+    items: Vec<T>,
+}
+
+impl<T> Queue<T> {
+    pub fn new() -> Queue<T> {
+        Queue { items: Vec::new() }
+    }
+
+    pub fn enqueue(&mut self, item: T) {
+        self.items.push(item);
+    }
+
+    pub fn dequeue(&mut self) -> Option<T> {
+        if self.is_empty() {
+            None
+        } else {
+            Some(self.items.remove(0))
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.items.len()
+    }
+}
+
+
+// let mut queue = Queue::new();
+
+// queue.enqueue(1);
+// queue.enqueue(2);
+// queue.enqueue(3);
+
+// assert_eq!(queue.len(), 3);
+
+// assert_eq!(queue.dequeue(), Some(1));
+// assert_eq!(queue.dequeue(), Some(2));
+// assert_eq!(queue.dequeue(), Some(3));
+
+// assert_eq!(queue.is_empty(), true);
